d056ba1d6ca0fe07041c9a5c287cc90145d8a642
sumitkrpat <sumit.kumar@anblicks.com>
2025-04-03 16:00:01 +0530

    added snyk

---
+++ GlobalNavService/packages/Selenium.WebDriver.ChromeDriver.2.8.0.0/tools/Init.ps1
@@ -0,0 +1,25 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$driverFile = "chromedriver.exe"
+$downloadUrl = "https://chromedriver.storage.googleapis.com/2.8/chromedriver_win32.zip"
+$contentPath = Join-Path $installPath "content"
+$driverPath = Join-Path $contentPath $driverFile
+
+if ((Test-Path $driverPath) -eq $false) {
+
+    # Download selenium driver zip file.
+    $tmpFilePath = [IO.Path]::GetTempFileName() + ".zip"
+    (new-object Net.WebClient).DownloadFile($downloadUrl, $tmpFilePath)
+
+    $shell = new-object -com Shell.Application
+    $zipFile = $shell.NameSpace($tmpFilePath)
+
+    $zipFile.Items() | `
+    where {(Split-Path $_.Path -Leaf) -eq $driverFile} | `
+    foreach {
+        $contentFolder = $shell.NameSpace($contentPath)
+        $contentFolder.copyhere($_.Path)
+    }
+
+    rm $tmpFilePath
+}
